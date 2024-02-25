c2ec195ee32ab93708dc36bd6cffbd9a53275030
vinitahj <vinitahj@gmail.com>
2024-02-25 23:21:24 +0530

    Update BaseController.php

--- backend/app/Controllers/BaseController.php
+++ backend/app/Controllers/BaseController.php
@@ -4,6 +4,17 @@ namespace App\Controllers;
 
 class BaseController
 {
+
+    public function __construct()
+    {
+        $this->setJsonHeader();
+    }
+
+    protected  function setJsonHeader()
+    {
+        header('Content-Type: application/json');
+    }
+
     public function index()
     {
         echo json_encode(['message' => 'Welcome to Address Book Application API']);
